a952be5f982c6d3045319fbe7ed26bf576bd628e
Fabian Sorqvist <fabian@sorqvist.nu>
2018-08-04 15:42:01 +0200

    now maybe (debugging on phone...)

--- wifi-sync.sh
+++ wifi-sync.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -e
-
-if echo $(termux-wifi-connectioninfo) | grep "COMPLETED"; then
+connected=$(termux-wifi-connectioninfo | grep "COMPLETED")
+echo $connected
+if [ $connected ]; then
     ./sync-folders.sh $1
 else
     echo "no wifi, not syncing"
