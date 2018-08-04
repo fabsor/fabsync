8ab790d543bc359f44aadff4feb48f9ad7f0662b
Fabian Sorqvist <fabian@sorqvist.nu>
2018-08-04 15:45:29 +0200

    more stars

--- wifi-sync.sh
+++ wifi-sync.sh
@@ -3,7 +3,7 @@
 set -e
 connected=$(termux-wifi-connectioninfo)
 
-if [[ $connected = *"CONNECTED" ]]; then
+if [[ $connected = *"CONNECTED"* ]]; then
     ./sync-folders.sh $1
 else
     echo "no wifi, not syncing"
